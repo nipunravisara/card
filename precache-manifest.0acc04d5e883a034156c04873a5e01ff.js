1fc82454d5bf158ddd15d8abf494601eb39f96e3
Your Name <you@example.com>
2019-04-02 00:29:09 +0530

    Updates

---
+++ precache-manifest.0acc04d5e883a034156c04873a5e01ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b280eb19e48950bec2fc67762b9e33c",
+    "url": "/card/static/media/profile.0b280eb1.png"
+  },
+  {
+    "revision": "ea07af475a06bddebb02",
+    "url": "/card/static/js/runtime~main.3c9c83e0.js"
+  },
+  {
+    "revision": "00de96ca9221a4c9ecc9",
+    "url": "/card/static/js/main.30048212.chunk.js"
+  },
+  {
+    "revision": "49fe2bb6337391b45054",
+    "url": "/card/static/js/2.6e39e941.chunk.js"
+  },
+  {
+    "revision": "00de96ca9221a4c9ecc9",
+    "url": "/card/static/css/main.7be0d032.chunk.css"
+  },
+  {
+    "revision": "9cfd432a2fe73d2958349256711307e6",
+    "url": "/card/index.html"
+  }
+];
